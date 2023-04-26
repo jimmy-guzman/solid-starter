2720db50e0972f1f781a37f79346f746d099ef23
jimmy-guzman <hi@jimmyguzman.com>
2023-04-25 20:12:51 -0500

    test: ✅ use `userEvent` v14 instead of v13

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,11 +1,11 @@
 import App from './App'
-import { render, userEvent, screen } from './test/utils'
+import { render, screen } from './test/utils'
 
 describe('app', () => {
   it('should increase count', async () => {
-    const { unmount } = render(() => <App />)
+    const { unmount, user } = render(() => <App />)
 
-    userEvent.click(screen.getByRole('button', { name: /count is 0/i }))
+    user.click(screen.getByRole('button', { name: /count is 0/i }))
 
     await expect(
       screen.findByRole('button', { name: /count is 1/i })
--- src/test/utils.ts
+++ src/test/utils.ts
@@ -1,4 +1,5 @@
 import { cleanup, render } from '@solidjs/testing-library'
+import userEvent from '@testing-library/user-event'
 import { afterEach } from 'vitest'
 
 import type { JSX } from 'solid-js/jsx-runtime'
@@ -7,14 +8,16 @@ afterEach(() => {
   cleanup()
 })
 
-const customRender = (ui: () => JSX.Element, options = {}) =>
-  render(ui, {
-    // wrap provider(s) here if needed
-    wrapper: ({ children }) => children,
-    ...options,
-  })
+const customRender = (ui: () => JSX.Element, options = {}) => {
+  return {
+    user: userEvent.setup(),
+    ...render(ui, {
+      wrapper: ({ children }) => children,
+      ...options,
+    }),
+  }
+}
 
 export * from '@solidjs/testing-library'
-export { default as userEvent } from '@testing-library/user-event'
 
 export { customRender as render }
